7c57e499b148f420627ca1c3a23c19cd51e4416d
msmakesof <msmakesof@gmail.com>
2020-05-11 13:30:22 -0500

    updates

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 # litigantes
 cnn Control Access
+<?php
+$hostname_cnn_kn = "litigantes.lawyer";
+$database_cnn_kn = "litigant_appjudicial";
+$username_cnn_kn = "litigant_makesof";
+$password_cnn_kn = "L1t1g4Nt3_fR33W4Y";
+$dominio="litigantes";
+?>
